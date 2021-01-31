4aa30d1cb6800061d43f4f171b3e2ddc350a1356
vasupronteff <vasudevan@pronteff.com>
2021-01-31 14:32:09 +0530

    cors issue

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
 var logger = require("morgan");
 require("dotenv").config();
 var indexRouter = require("./routes/index");
@@ -33,13 +34,42 @@ var app = express();
 if (process.env.NODE_ENV !== "test") {
   app.use(logger("dev"));
 }
-app.use(express.json());
+app.use(express.json({ limit: "2gb", extended: true }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.json({ limit: 1024 * 1024 * 20, type: "application/json" }));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+    limit: 1024 * 1024 * 20,
+    type: "application/x-www-form-urlencoding",
+  })
+);
 app.use(express.static(path.join(__dirname, "public")));
 
 //To allow cross-origin requests
 app.use(cors());
+
+// CORS middleware
+app.use((req, res, next) => {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  // Pass to next layer of middleware
+  next();
+});
 
 //add file upload directory
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
