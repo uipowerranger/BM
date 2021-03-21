b2261d5cf626194afcd3ce0e6cf9692bfbabde0d
vasupronteff <vasudevan@pronteff.com>
2021-03-21 14:05:56 +0530

    cron jobs

--- helpers/mailer.js
+++ helpers/mailer.js
@@ -1,5 +1,5 @@
 const nodemailer = require("nodemailer");
-
+const { constants } = require("./constants");
 // create reusable transporter object using the default SMTP transport
 let transporter = nodemailer.createTransport({
   service: process.env.EMAIL_SMTP_HOST,
@@ -17,6 +17,19 @@ exports.send = function (from, to, subject, html) {
   return transporter.sendMail({
     from: `"BirlaMart" ${from}`, // sender address e.g. no-reply@xyz.com or "Fred Foo 👻" <foo@example.com>
     to: to, // list of receivers e.g. bar@example.com, baz@example.com
+    subject: subject, // Subject line e.g. 'Hello ✔'
+    //text: text, // plain text body e.g. Hello world?
+    html: html, // html body e.g. '<b>Hello world?</b>'
+  });
+};
+
+exports.sendReport = function (to, cc, subject, html) {
+  // send mail with defined transport object
+  // visit https://nodemailer.com/ for more options
+  return transporter.sendMail({
+    from: `"BirlaMart" ${constants.confirmEmails.from}`, // sender address e.g. no-reply@xyz.com or "Fred Foo 👻" <foo@example.com>
+    to: to, // list of receivers e.g. bar@example.com, baz@example.com
+    cc: cc,
     subject: subject, // Subject line e.g. 'Hello ✔'
     //text: text, // plain text body e.g. Hello world?
     html: html, // html body e.g. '<b>Hello world?</b>'
